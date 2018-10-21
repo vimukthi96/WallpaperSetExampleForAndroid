a8925fff7566edb00b3a9ad509a1e49bf5b54272
vimukthi96 <vimukthi072@gmail.com>
2018-10-21 20:59:08 +0530

    Set search bar correctly

--- app/src/main/java/com/example/vimukthi/wallpapersetexampleforandroid/MainActivity.java
+++ app/src/main/java/com/example/vimukthi/wallpapersetexampleforandroid/MainActivity.java
@@ -1,15 +1,20 @@
 package com.example.vimukthi.wallpapersetexampleforandroid;
 
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +37,53 @@ public class MainActivity extends AppCompatActivity {
 
         firebaseDatabase =FirebaseDatabase.getInstance();
         databaseReference =firebaseDatabase.getReference("Data");
+    }
+
+    //search data
+    private  void firebaseSearch(String searchText){
+        Query firebaseSearchQuery =databaseReference.orderByChild("title").startAt(searchText).endAt(searchText +"\uf8ff");
+        FirebaseRecyclerAdapter<Model,ViewHolder> firebaseRecyclerAdapter=
+                new FirebaseRecyclerAdapter<Model, ViewHolder>(
+                        Model.class,R.layout.row,ViewHolder.class,databaseReference
+                ) {
+                    @Override
+                    protected void populateViewHolder(ViewHolder viewHolder, Model model, int position) {
+                        viewHolder.setDetails(getApplicationContext(),model.getTitle(),model.getDescription(),model.getImage());
+
+                    }
+                };
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        MenuItem item =menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                firebaseSearch(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                firebaseSearch(newText);
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id =item.getItemId();
+        if(id == R.id.action_setting){
+            //TODO
+            return  true; 
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
