bc10cfad3f199e1d469a27fd985e8688b0f2ae49
vimukthi96 <vimukthi072@gmail.com>
2018-10-21 20:15:17 +0530

    Set recycle view correctly

--- app/src/main/java/com/example/vimukthi/wallpapersetexampleforandroid/MainActivity.java
+++ app/src/main/java/com/example/vimukthi/wallpapersetexampleforandroid/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.vimukthi.wallpapersetexampleforandroid;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Display;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar =getSupportActionBar();
+        actionBar.setTitle("Posts List");
+
+        recyclerView =findViewById(R.id.recycleView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        firebaseDatabase =FirebaseDatabase.getInstance();
+        databaseReference =firebaseDatabase.getReference("Data");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseRecyclerAdapter<Model,ViewHolder> firebaseRecyclerAdapter =new FirebaseRecyclerAdapter<Model, ViewHolder>(
+                Model.class,R.layout.row,ViewHolder.class,databaseReference
+        ) {
+            @Override
+            protected void populateViewHolder(ViewHolder viewHolder, Model model, int position) {
+                viewHolder.setDetails(getApplicationContext(),model.getTitle(),model.getDescription(),model.getImage());
+            }
+        };
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 }
